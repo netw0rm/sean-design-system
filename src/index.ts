f27c2097993201cc21cb29650863567261bd0dde
sean <qiusct@gmail.com>
2022-11-18 19:27:32 +0800

    component

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+
+console.log('hello');
+
+export default {}
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()]
+// })
+
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: 'sean-design-system',
+    },
+    sourcemap: true,
+    RollupOptions:{}
+  },
+  css: {}
 })
